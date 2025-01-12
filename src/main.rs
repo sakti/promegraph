328fbb18c2e3c2ab6abcd83117f186ff2993d702
Sakti Dwi Cahyono <me@saktidwicahyono.name>
2025-01-12 20:05:37 +0700

    add clap
    Signed-off-by: Sakti Dwi Cahyono <me@saktidwicahyono.name>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use anyhow::Result;
+use clap::Parser;
+
 mod promql;
 
 use promql::get_data;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Promql expression
+    #[arg(short, long)]
+    expr: String,
+
+    /// Prometheus server address
+    #[arg(short, long)]
+    addr: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args = Args::parse();
+    dbg!(args);
     println!("promegraph");
 
     println!("{}", get_data("up").await.unwrap());
