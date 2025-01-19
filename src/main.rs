b3b9630abee184803117a39a43d13851f63f2b9a
Sakti Dwi Cahyono <me@saktidwicahyono.name>
2025-01-19 16:57:40 +0700

    feat: implement textplots
    Signed-off-by: Sakti Dwi Cahyono <me@saktidwicahyono.name>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use clap::{Parser, ValueEnum};
+use jiff::{tz::TimeZone, Timestamp};
 use miette::Result;
 
 mod promql;
 
 use promql::get_data;
-use textplots::{Chart, Plot, Shape};
+use textplots::{Chart, LabelBuilder, LabelFormat, Plot, Shape, TickDisplay, TickDisplayBuilder};
 
 #[derive(ValueEnum, Clone, Debug)]
 enum Backend {
@@ -23,27 +24,97 @@ struct Args {
     #[arg(short, long, default_value = "http://localhost:8428/")]
     addr: String,
 
+    /// Step
+    #[arg(short, long, default_value_t = 15.0)]
+    step: f64,
+
+    /// Duration in minutes
+    #[arg(short, long, default_value_t = 1)]
+    duration: u8,
+
     #[arg(short,value_enum, default_value_t = Backend::Textplots)]
     backend: Backend,
+}
+
+fn get_bounds(points: &[(f64, f64)]) -> (f64, f64, f64, f64) {
+    if points.is_empty() {
+        return (0.0, 0.0, 0.0, 0.0);
+    }
+
+    let mut xmin = points[0].0;
+    let mut xmax = points[0].0;
+    let mut ymin = points[0].1;
+    let mut ymax = points[0].1;
+
+    for &(x, y) in points.iter() {
+        xmin = xmin.min(x);
+        xmax = xmax.max(x);
+        ymin = ymin.min(y);
+        ymax = ymax.max(y);
+    }
+
+    (xmin, xmax, ymin, ymax)
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
-    dbg!(&args);
-    println!("promegraph");
     match args.backend {
         Backend::Plotters => {
             println!("using plotters");
-            println!("{}", get_data(&args.addr, "up").await?);
+            get_data(&args.addr, &args.expr, args.step, args.duration).await?;
         }
         Backend::Textplots => {
-            println!("using textplots");
-            Chart::default()
-                .lineplot(&Shape::Continuous(Box::new(|x| x.sin() / x)))
-                .display();
+            let result = get_data(&args.addr, &args.expr, args.step, args.duration).await?;
+            for (_i, v) in result.iter().enumerate() {
+                let _metric_name = v.metric().get("__name__").cloned().unwrap_or_default();
+                let points: Vec<(f64, f64)> = v
+                    .samples()
+                    .into_iter()
+                    .map(|s| (s.timestamp(), s.value()))
+                    .collect();
+                let (_xmin, _xmax, ymin, ymax) = get_bounds(&points);
 
-            println!("{}", get_data(&args.addr, "up").await?);
+                let vec32: Vec<(f32, f32)> =
+                    points.iter().map(|(x, y)| (*x as f32, *y as f32)).collect();
+
+                println!("{}", args.expr);
+                Chart::new_with_y_range(
+                    200,
+                    60,
+                    0.0,
+                    points.len() as f32,
+                    (ymin - 0.01) as f32,
+                    (ymax + 0.01) as f32,
+                )
+                .lineplot(&Shape::Continuous(Box::new(|x| {
+                    let idx = x as usize;
+                    if idx < vec32.len() {
+                        vec32[idx].1
+                    } else {
+                        0.0
+                    }
+                })))
+                .x_label_format(LabelFormat::Custom(Box::new(move |val| {
+                    let idx = val as usize;
+                    if idx < points.len() {
+                        let ts = Timestamp::from_second(points[idx].0 as i64).unwrap();
+                        ts.to_zoned(TimeZone::system()).to_string()
+                        // format!("{}", points[idx].0)
+                    } else {
+                        // get last value
+                        // format!("{}", points[points.len() - 1].0)
+                        let ts = Timestamp::from_second(points[points.len() - 1].0 as i64).unwrap();
+                        ts.to_zoned(TimeZone::system()).to_string()
+                        // format!("{}", val as usize)
+                    }
+                    // format!("{}", points[val as usize].0)
+                    // format!("{}", val as usize)
+                })))
+                .y_label_format(LabelFormat::Value)
+                .y_tick_display(TickDisplay::Sparse)
+                .display();
+            }
             println!();
         }
     }
--- src/promql.rs
+++ src/promql.rs
@@ -1,18 +1,24 @@
 use jiff::Timestamp;
-use miette::{IntoDiagnostic, Result};
-use prometheus_http_query::Client;
+use miette::{miette, IntoDiagnostic, Result};
+use prometheus_http_query::{response::RangeVector, Client};
 
-pub async fn get_data(addr: &str, expr: &str) -> Result<String> {
+pub async fn get_data(addr: &str, expr: &str, step: f64, duration: u8) -> Result<Vec<RangeVector>> {
     let client = Client::try_from(addr).into_diagnostic()?;
-    let start = Timestamp::now().as_second() - (5 * 60);
+    let start = Timestamp::now().as_second() - (duration as i64 * 60);
     let end = Timestamp::now().as_second();
-    dbg!(start);
-    dbg!(end);
     let response = client
-        .query_range(expr, start, end, 60.0)
+        .query_range(expr, start, end, step)
         .get()
         .await
         .into_diagnostic()?;
-    dbg!(response);
-    Ok("the data".to_string())
+    let result_found = response.data().as_matrix().is_some();
+    if !result_found {
+        return Err(miette!("range vector not found"));
+    }
+    let result = response.data().as_matrix().unwrap();
+    if result.is_empty() {
+        return Err(miette!("empty result"));
+    }
+
+    Ok(result.to_vec())
 }
